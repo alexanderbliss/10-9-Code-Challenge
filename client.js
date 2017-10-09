83f8dbe60282292d19db6eb65b42201bbe94fe53
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-09 14:21:26 -0500

    completed buttons and swapping stlyes, but needs refignment

--- client.js
+++ client.js
@@ -3,15 +3,27 @@
  function readyNow(){
    console.log('script and jquery sourced');
   $('#gennerate').on('click' , generateClick);
+
 }
 
 function generateClick(){
   console.log('click');
   $('.container').append('<div class=clicks>' + "Button Clicked" + '</div>');
   genButton();
+
 }
 
 function genButton(){
   $('.clicks').append('<button class=swap>Swap</button>');
   $('.clicks').append('<button class=delete>Delete</button>');
+$('.swap').on('click', swapColor);
+$('.delete').on('click' , deleteBtn);
+
+}
+function swapColor(){
+  $('.clicks').css('background-color', 'yellow');
+  console.log('swap');
+}
+function deleteBtn(){
+console.log('delete');
 }
