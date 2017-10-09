92e1299e370a9d206bccc294095134e27ca8d8c6
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-09 14:02:04 -0500

    appending dom on click

--- client.js
+++ client.js
@@ -2,5 +2,10 @@
 
  function readyNow(){
    console.log('script and jquery sourced');
+  $('#gennerate').on('click' , generateClick);
+}
 
- }
+function generateClick(){
+  console.log('click');
+  $('.container').append('<div class=clicks>' + "Button Clicked" + '</div>');
+}
