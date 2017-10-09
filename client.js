c62b7762cc5729621d2bdec3fdd8fc4f34f041db
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-09 15:06:55 -0500

    submitting assignment

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
- $(document).ready(readyNow);
+$(document).ready(readyNow);
 
- function readyNow(){
-   console.log('script and jquery sourced');
+function readyNow(){
+  console.log('script and jquery sourced');
   $('#gennerate').on('click' , generateClick);
 
 }
@@ -9,23 +9,19 @@
 function generateClick(){
   console.log('click');
   $('.container').append('<div class=clicks></div>');
-  genButton();
-
-}
-
-function genButton(){
   $('.clicks').append('<button class="swap">Swap</button>');
   $('.clicks').append('<button class="delete">Delete</button>');
-$('.swap').on('click', swapColor);
-$('.delete').on('click' , deleteBtn);
-$tr.append('<button class="deleteBtn">Delete</button>');
+  $('.swap').on('click', swapColor);
+  $('.delete').on('click' , deleteBtn);
+
 }
 function swapColor(){
+
   $(this).parent().css('background-color', 'yellow');
 
   console.log('swap');
 }
 function deleteBtn(){
-$(this).parent().remove();
-console.log('delete');
+  $(this).parent().remove();
+  console.log('delete');
 }
